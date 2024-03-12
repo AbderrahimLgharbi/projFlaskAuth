0a78f564223b47ea0c8b28c5a2295ef3f1c57315
AbderrahimLgharbi <124380096+AbderrahimLgharbi@users.noreply.github.com>
2024-03-12 12:30:57 +0000

    first-commit-index

---
+++ pythonProj/app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, redirect, url_for,session,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, ValidationError
+import bcrypt
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pydb1'
+app.secret_key = 'your_secret_key_here'
+
+mysql = MySQL(app)
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+    submit = SubmitField("Register")
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login',methods=['GET','POST'])
+def Login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        # store data into database
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE email=%s",(email,))
+        user=cursor.fetchone()
+        cursor.close()
+        if user and bcrypt.checkpw(password.encode('utf-8'),user[3].encode('utf-8')):
+            session['user_id']=user[0]
+            return redirect(url_for('Dashbord'))
+        else:
+            flash('Login Failled')
+            return redirect(url_for('Login'))
+
+    return render_template('login.html',form=form )
+    
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+
+        hashed_password = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt())
+
+        # store data into database
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO users (name,email,password) VALUES (%s,%s,%s)",
+                    (name, email, hashed_password))
+        mysql.connection.commit()
+        cursor.close()
+
+        return redirect(url_for('Login'))
+
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/dashboard')
+def Dashbord():
+    return render_template('dashboard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5011)
+
+
+
